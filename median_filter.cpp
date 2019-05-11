9026e30a8a42d336393644ee9a3a4198e937ba75
Nlioxa Korzun <36415737+Nlioxa@users.noreply.github.com>
2019-05-12 01:44:26 +0300

    Update median_filter.cpp

--- median_filter.cpp
+++ median_filter.cpp
@@ -9,8 +9,8 @@ void median_filter_(type* signal, int signal_size, int window_size)
 	type** _window = new type*[window_size];
 
 	// construct a window for the first element of the signal 
-	for( auto ( cell ) { window }, ( median_cell ) { window + window_size / 2 };
-		cell < window + window_size; cell++ )
+	for( auto ( cell ) { window }, ( median_cell ) { window + window_size / 2 }, ( end_cell ) { window + window_size - 1 };
+		cell <= end_cell; cell++ )
 	{
 		if( cell < median_cell )
 		{
@@ -18,7 +18,8 @@ void median_filter_(type* signal, int signal_size, int window_size)
 		}
 		else
 		{
-			*cell = *( signal + ( cell - median_cell ) );
+			auto ( indent ) { cell - median_cell };
+			*cell = *( signal + indent );
 		}
 	}
 
@@ -32,6 +33,7 @@ void median_filter_(type* signal, int signal_size, int window_size)
 	sort(_window, _window + window_size,
 		[](type* a, type* b) {return *a < *b; });
 
+	// set a pointer to the median cell of the sorted window
 	auto ( median_cell ) { _window + window_size / 2 };
 
 	signal[0] = **median_cell;
@@ -39,8 +41,9 @@ void median_filter_(type* signal, int signal_size, int window_size)
 	for( auto ( current_element ) { signal }, ( first_element ) { signal }, ( last_element ) { signal + signal_size - 1 }, ( dated_cell ) { window };
 		current_element < last_element; current_element++ )
 	{
-		auto ( next_element ) { current_element + static_cast<int>( ceil(window_size / 2.0) ) };
-		auto next_value = next_element <= last_element ? *next_element : 0;
+		auto ( indent ) { static_cast<int>( ceil(window_size / 2.0) ) };
+		auto ( next_element ) { current_element + indent };
+		auto ( next_value ) { next_element <= last_element ? *next_element : 0 };
 		*dated_cell = next_value;
 		dated_cell = dated_cell + 1 < window + window_size ? dated_cell + 1 : window;
 		sort(_window, _window + window_size,
