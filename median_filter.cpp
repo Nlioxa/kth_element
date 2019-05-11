6d9f0c8b71bd155b0324c9696b84ffeb9f1bfe2d
Nlioxa Korzun <36415737+Nlioxa@users.noreply.github.com>
2019-05-12 01:34:54 +0300

    Create median_filter.cpp

---
+++ median_filter.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+
+template<class type>
+void median_filter_(type* signal, int signal_size, int window_size)
+{
+	using namespace std;
+
+	type* window = new type[window_size];
+	type** _window = new type*[window_size];
+
+	// construct a window for the first element of the signal 
+	for( auto ( cell ) { window }, ( median_cell ) { window + window_size / 2 };
+		cell < window + window_size; cell++ )
+	{
+		if( cell < median_cell )
+		{
+			*cell = 0;
+		}
+		else
+		{
+			*cell = *( signal + ( cell - median_cell ) );
+		}
+	}
+
+	// assign pointer to each cell of the window
+	for( auto ( index ) { 0 }; index < window_size; index++ )
+	{
+		_window[index] = &window[index];
+	}
+
+	// the first and the least sort of the window
+	sort(_window, _window + window_size,
+		[](type* a, type* b) {return *a < *b; });
+
+	auto ( median_cell ) { _window + window_size / 2 };
+
+	signal[0] = **median_cell;
+
+	for( auto ( current_element ) { signal }, ( first_element ) { signal }, ( last_element ) { signal + signal_size - 1 }, ( dated_cell ) { window };
+		current_element < last_element; current_element++ )
+	{
+		auto ( next_element ) { current_element + static_cast<int>( ceil(window_size / 2.0) ) };
+		auto next_value = next_element <= last_element ? *next_element : 0;
+		*dated_cell = next_value;
+		dated_cell = dated_cell + 1 < window + window_size ? dated_cell + 1 : window;
+		sort(_window, _window + window_size,
+			[](type* a, type* b) {return *a < *b; });
+		*( current_element + 1 ) = window_size % 2 ? **median_cell : ( **median_cell + **( median_cell - 1 ) ) / 2;
+	}
+}
